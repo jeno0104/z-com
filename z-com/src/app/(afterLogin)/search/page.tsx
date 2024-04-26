d0b0546c496d9d03adf7761e9d0d5bb94875eda8
zenoo97 <jaehun4086@naver.com>
2024-04-26 20:47:49 +0900

    [Update] 탐색하기 컴포넌트

--- z-com/src/app/(afterLogin)/search/page.tsx
+++ z-com/src/app/(afterLogin)/search/page.tsx
@@ -1,3 +1,28 @@
-export default function Search() {
-  return <main>asd</main>;
+import BackButton from "../_component/BackButton";
+import SearchForm from "../_component/SearchForm";
+import Tab from "./_component/Tab";
+import style from "./search.module.css";
+
+type Props = {
+  searchParams: { q: string; f?: string; pf?: string };
+};
+export default function Search({ searchParams }: Props) {
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        {/* <SearchResult searchParams={searchParams} /> */}
+      </div>
+    </main>
+  );
 }
